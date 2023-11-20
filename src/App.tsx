535f9051a6e382dde806e4f77d01ba86ca32eb54
mechapear <mechapear@gmail.com>
2023-11-20 15:59:50 +0700

    refactor: use useEffect to handle time counting

--- src/App.tsx
+++ src/App.tsx
@@ -1,53 +1,48 @@
 import './App.css'
-import { useEffect, useRef, useState } from 'react'
+import { useEffect, useState } from 'react'
 
 export default function App() {
-  const [isSCounting, setIsCounting] = useState(false)
+  const [isCounting, setIsCounting] = useState(false)
   const [timeDuration, setTimeDuration] = useState(0)
-  // store intervalID
-  const intervalIDRef = useRef<number | undefined>(undefined)
 
-  // prevent memory leak
-  // clear interval when component unmount
   useEffect(() => {
     console.log('useEffect')
-    // clean up function
+    let interval: number | undefined = undefined
+
+    if (isCounting) {
+      // creat new interval andd store in intervalIDRef
+      // myInterval = setInterval(function, milliseconds)
+      interval = setInterval(() => {
+        setTimeDuration((prevTimeDuration) => {
+          console.log(`prevTimeDuration: `, prevTimeDuration)
+          return prevTimeDuration + 1
+        })
+      }, 1000)
+    } else {
+      clearInterval(interval)
+    }
+
+    // prevent memory leak
+    // clear interval when component unmount
     return () => {
-      clearInterval(intervalIDRef.current)
+      clearInterval(interval)
       console.log('cleanup')
     }
-  }, [])
+  }, [isCounting]) // only run when isCounting state change
 
   function handleStart() {
-    // clear interval before start
-    clearInterval(intervalIDRef.current)
-
     setIsCounting(true)
     console.log('start')
-
-    // creat new interval andd store in intervalIDRef
-    // myInterval = setInterval(function, milliseconds)
-    intervalIDRef.current = setInterval(() => {
-      // Update setTimeDuration state multiple times in one event
-      setTimeDuration((prevTimeDuration) => {
-        console.log(`prevTimeDuration: `, prevTimeDuration)
-        return prevTimeDuration + 1
-      })
-    }, 1000)
   }
 
   function handlePause() {
     setIsCounting(false)
-    // cancel existing interval
-    clearInterval(intervalIDRef.current)
     console.log('pause')
   }
 
   function handleReset() {
     setIsCounting(false)
     setTimeDuration(0)
-    clearInterval(intervalIDRef.current)
-    intervalIDRef.current = undefined
     console.log('stop')
   }
 
@@ -58,7 +53,7 @@ export default function App() {
         <span>00:</span>
         <span>{timeDuration}</span>
       </div>
-      {isSCounting ? (
+      {isCounting ? (
         <button onClick={handlePause}>Pause</button>
       ) : (
         <button onClick={handleStart}>Start</button>
